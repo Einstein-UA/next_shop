f5ac21771c61bce827b0b4fb6a5d053965846f3b
Ruslan-UA <mnemonikus69@gmail.com>
2023-08-25 18:30:30 +0300

    fixing global page reload in deploy

--- next.config.js
+++ next.config.js
@@ -2,4 +2,12 @@ module.exports = {
     images: {
         domains: ['i.dummyjson.com'],
       },
+      async rewrites() {
+        return [
+          {
+            source: '/:path*',
+            destination: '/',
+          },
+        ]
+      },
 }
