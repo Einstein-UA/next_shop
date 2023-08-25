303b2e30b01a30c9b4fa485aed426b4dc94f9ae6
Ruslan-UA <mnemonikus69@gmail.com>
2023-08-25 19:03:38 +0300

    test deploy reload page

--- next.config.js
+++ next.config.js
@@ -2,32 +2,4 @@ module.exports = {
     images: {
         domains: ['i.dummyjson.com'],
       },
-      async rewrites() {
-        return [
-          {
-            source: '/aboutUs.html',
-            destination: '/aboutUs.html',
-          },
-          {
-            source: '/cart.html',
-            destination: '/cart.html',
-          },
-          {
-            source: '/contactUs.html',
-            destination: '/contactUs.html',
-          },
-          {
-            source: '/index.html',
-            destination: '/index.html',
-          },
-          {
-            source: '/login.html',
-            destination: '/login.html',
-          },
-          {
-            source: '/products.html',
-            destination: '/products.html',
-          },
-        ]
-      },
 }
