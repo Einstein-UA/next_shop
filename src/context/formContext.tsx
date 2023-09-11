9920c659cf0982ed5af899cf75b4153e2bdebe13
Ruslan-UA <mnemonikus69@gmail.com>
2023-09-11 21:20:40 +0300

    minor changes

--- src/context/formContext.tsx
+++ src/context/formContext.tsx
@@ -1,45 +1,71 @@
 'use client'
 import React, {useContext, createContext, useState} from "react";
-interface FormValues {
-    Name: string;
-    Email: string;
-    Message: string;
-    Password: string;
-}
+
 interface FormContextProps {
     isSubmitted: boolean,
-    setSubmitted: React.Dispatch<React.SetStateAction<boolean>>,
-    formData: FormValues,
-    setFormData: React.Dispatch<React.SetStateAction<object>>,
+    setSubmitted: React.Dispatch<React.SetStateAction<boolean>>
 }
-
 
 const FormContext = createContext<FormContextProps>({
     isSubmitted: false,
     setSubmitted: () => {},
-    formData: {
-        Name: '',
-        Email: '',
-        Message: '',
-        Password: '',
-    },
-    setFormData: () => {},
-
 })
 
 export const FormContextProvider = ({children}: any) => {
     const [isSubmitted, setSubmitted] = useState(false)
-    const [formData, setFormData] = useState({
-        Name: "",
-        Email: "",
-        Message: "",
-        Password: "",
-    });
+
     return (
-        <FormContext.Provider value={{isSubmitted, setSubmitted, formData, setFormData}}>
+        <FormContext.Provider value={{isSubmitted, setSubmitted}}>
             {children}
         </FormContext.Provider>
     )
 }
 
 export const useFormContext = () => useContext(FormContext)
+
+
+// 'use client'
+// import React, {useContext, createContext, useState} from "react";
+// interface FormValues {
+//     Name: string;
+//     Email: string;
+//     Message: string;
+//     Password: string;
+// }
+// interface FormContextProps {
+//     isSubmitted: boolean,
+//     setSubmitted: React.Dispatch<React.SetStateAction<boolean>>,
+//     formData: FormValues,
+//     setFormData: React.Dispatch<React.SetStateAction<object>>,
+// }
+//
+//
+// const FormContext = createContext<FormContextProps>({
+//     isSubmitted: false,
+//     setSubmitted: () => {},
+//     formData: {
+//         Name: '',
+//         Email: '',
+//         Message: '',
+//         Password: '',
+//     },
+//     setFormData: () => {},
+//
+// })
+//
+// export const FormContextProvider = ({children}: any) => {
+//     const [isSubmitted, setSubmitted] = useState(false)
+//     const [formData, setFormData] = useState({
+//         Name: "",
+//         Email: "",
+//         Message: "",
+//         Password: "",
+//     });
+//     return (
+//         <FormContext.Provider value={{isSubmitted, setSubmitted, formData, setFormData}}>
+//             {children}
+//         </FormContext.Provider>
+//     )
+// }
+//
+// export const useFormContext = () => useContext(FormContext)
